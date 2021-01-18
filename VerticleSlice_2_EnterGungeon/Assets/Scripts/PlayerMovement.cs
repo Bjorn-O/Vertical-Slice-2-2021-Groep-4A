ec04657d1c22e91757e1b019f35e2ed6596b16e4
Daan Kikkert <54845837+DaanKikkert@users.noreply.github.com>
2021-01-18 11:49:05 +0100

    basic player movement

---
+++ VerticleSlice_2_EnterGungeon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody _rigidbody;
+    [SerializeField] float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+    }
+}
