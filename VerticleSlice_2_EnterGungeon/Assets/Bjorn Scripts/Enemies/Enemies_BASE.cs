2ed16c5d2683c7b31320c8c2fc29b997138f3a6a
Björn <Bjorn.oudkerk@outlook.com>
2021-01-18 11:47:15 +0100

    Simple Line of Sight check

---
+++ VerticleSlice_2_EnterGungeon/Assets/Bjorn Scripts/Enemies/Enemies_BASE.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies_BASE : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _minRange;
+
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ VerticleSlice_2_EnterGungeon/Assets/Bjorn Scripts/Enemies/LoS_Checker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoS_Checker : MonoBehaviour
+{
+    [SerializeField] private string _targetTag;
+    private GameObject _targetedPlayer;
+    private Vector3 _targetDirection;
+
+    private bool isVisible;
+
+    void FixedUpdate()
+    {
+        _targetedPlayer = GameObject.FindGameObjectWithTag("Player");
+        if (_targetedPlayer)
+        {
+            _targetDirection = (transform.position - _targetedPlayer.transform.position) * -1;
+        }
+
+        Ray ray = new Ray (transform.position, _targetDirection);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.tag == _targetTag)
+            {
+                Debug.DrawRay(ray.origin, _targetDirection , Color.red);
+                isVisible = true;
+            } else
+            {
+                Debug.DrawRay(ray.origin, _targetDirection, Color.green);
+                isVisible = false;
+            }
+        }
+    }
+}
