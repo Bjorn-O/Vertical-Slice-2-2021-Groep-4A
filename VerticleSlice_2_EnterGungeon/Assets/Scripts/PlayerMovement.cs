2de77e2491e0a870e32329d4bb025e4e81e2b057
Daan Kikkert <54845837+DaanKikkert@users.noreply.github.com>
2021-01-21 09:30:08 +0100

    aaaaa

--- VerticleSlice_2_EnterGungeon/Assets/Scripts/PlayerMovement.cs
+++ VerticleSlice_2_EnterGungeon/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody _rigidbody;
     [SerializeField] float moveSpeed;
+    Vector3 moveDirection;
+    Rigidbody rigidbody;
+    [SerializeField] float rollSpeed = 80;
+    private bool isRolling = false;
+    [SerializeField] private float rollTimer = 0.7f;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+        if (isRolling == false)
+        {
+            rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        }
+
+        moveDirection = rigidbody.velocity;
+
+        Debug.Log(moveDirection);
+        if (Input.GetKeyDown(KeyCode.Space) && isRolling == false && rigidbody.velocity != Vector3.zero)
+        {
+            StartCoroutine("Roll");
+        }
+    }
+
+    private IEnumerator Roll()
+    {
+        isRolling = true;
+        rigidbody.velocity = moveDirection.normalized * rollSpeed;
+        yield return new WaitForSeconds(rollTimer);
+        rigidbody.velocity = Vector3.zero;        
+        isRolling = false;
     }
 }
+
