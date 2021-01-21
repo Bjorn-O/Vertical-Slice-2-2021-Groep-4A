b4f6c765f636c62b73c82b83ca080dc1aa94804a
Ruben Dudink <rubendudink2003@gmail.com>
2021-01-21 10:03:08 +0100

    Aiming mechanic for guns
    help

---
+++ VerticleSlice_2_EnterGungeon/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+  
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+}
